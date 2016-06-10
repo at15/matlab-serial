2abff345dea64764cd76251562cfbdbffdc21665
unknown <at15@outlook.com>
2016-06-10 16:38:17 +0800

    [gui] add a edit ctrl to show read data

--- gui/simple.m
+++ gui/simple.m
@@ -22,7 +22,7 @@ function varargout = simple(varargin)
 
 % Edit the above text to modify the response to help simple
 
-% Last Modified by GUIDE v2.5 10-Jun-2016 15:42:58
+% Last Modified by GUIDE v2.5 10-Jun-2016 16:31:18
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -136,7 +136,10 @@ try
     
     % read by bytes length, NOTE: char(out') 
     out = fread(s,n,'char');
-    disp(char(out'));
+    str = char(out');
+    disp(str);
+    % show on edit control
+     set(handles.text_read, 'string', str);
 catch ex
     disp(ex.message);
 end
@@ -171,3 +174,24 @@ end
 guidata(hObject,handles);
 
 
+
+function text_read_Callback(hObject, eventdata, handles)
+% hObject    handle to text_read (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of text_read as text
+%        str2double(get(hObject,'String')) returns contents of text_read as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function text_read_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to text_read (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
